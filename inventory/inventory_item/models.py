2e2bddb170e4c3b3ad36285dc8efc09719da9ab5
yuchen Zeng <yuchenzeng@yuchens-MacBook-Pro.local>
2022-01-16 21:08:33 -0600

    add reference

--- inventory/inventory_item/models.py
+++ inventory/inventory_item/models.py
@@ -3,7 +3,6 @@ from pyexpat import model
 from unicodedata import category
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     quantity = models.PositiveIntegerField(null=True)
